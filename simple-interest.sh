57696cd3939922d85a8d709c990fff7cd77b4434
Suyash-C <156316040+Suyash-C@users.noreply.github.com>
2024-06-07 16:06:08 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash
+
+# Simple interest calculation
+principal=1000
+rate=0.05
+time=2
+
+interest=$(echo "scale=2; $principal * $rate * $time" | bc)
+
+echo "Simple Interest: $interest"
